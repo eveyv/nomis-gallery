260ead5b0818babe7835997a766543618c235eee
Everett Yeaw <evyeaw@gmail.com>
2021-03-16 11:06:56 -0400

    basic structure for pic data

---
+++ src/photo-data.js
@@ -0,0 +1,47 @@
+const photos = [
+    {
+        id: 0,
+        url: "",
+        name:""
+    },
+    {
+        id: 1,
+        url: "",
+        name:"",
+    },
+    {
+        id: 2,
+        url: "",
+        name: ""
+    },
+    {
+        id: 3,
+        url: "",
+        name: ""
+    },
+    {
+        id: 4,
+        url: "",
+        name:""
+    },
+    {
+        id: 5,
+        url: "",
+        name: ""
+    },
+    {
+        id: 6,
+        url: "",
+        name: ""
+    },
+    {
+        id: 7,
+        url: "",
+        name: ""
+    },
+    {
+        id: 8,
+        url: "",
+        name: ""
+    },
+]
